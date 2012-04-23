d3438997ff7c0490da0cd08649fde60d6d39aab9
Matthieu Vachon <matthieu.o.vachon@gmail.com>
2012-04-22 20:27:01 -0400

    - Removed assertion since it is not required to test this bug

--- src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
+++ src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
@@ -9,9 +9,6 @@ class DemoControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createClient();
-
         $crawler = $client->request('GET', '/demo/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
     }
 }
